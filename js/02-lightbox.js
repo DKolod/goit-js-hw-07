98933c9c30b2dc5c6d1994f111b836686ae8269a
Dmitriy Kolodchenko <dima.kolod@gmail.com>
2022-05-04 14:41:53 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+const gallaryList = makeGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', gallaryList)
+
+function makeGalleryItems(items) {
+    return items.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href='${original}'>
+         <img
+      class="gallery__image"
+      src='${preview}' 
+      data-source='${original}
+      alt='${description}'
+    />
+  </a>
+</div>`
+    })
+    .join('');
+}
+const galleryBox = new SimpleLightbox('.gallery a', {
+    /* options */
+    captionDelay: 250,
+    captionsData: 'alt'
+
+});
